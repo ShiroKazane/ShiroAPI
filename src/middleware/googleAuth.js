227825547f3afed24a2480aab28b723124927190
Shiro <bayuofficial773@gmail.com>
2023-02-04 14:10:28 +0700

    Redesign Color Combination

--- src/middleware/googleAuth.js
+++ src/middleware/googleAuth.js
@@ -2,9 +2,10 @@ const { whitelists } = require('../configs/whitelists.json');
 
 module.exports = (req, res, next) => {
 	if (!req.user) return res.status(401).redirect('/auth/login');
-	if (whitelists.includes(req.user.id)) {
-		next();
-	} else {
-		res.status(401).render('4xx/401');
+
+	if (!whitelists.includes(req.user.id)) {
+		return res.status(401).render('4xx/401');
 	}
+
+	next();
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const router = new express.Router();
+const router = express.Router();
 const googleAuth = require('../middleware/googleAuth');
 
 router.get('/', googleAuth, (req, res) => {
-	const username = req.user.displayName;
-	const email = req.user._json.email;
-	const id = req.user.id;
-	var avatarURL = req.user._json.picture;
-	avatarURL = avatarURL.replace(/=s96-c/g, '?rel=0');
-	res.render('dashboard', {
-		avatar: avatarURL,
-		username,
-		email,
-        id
-	});
-});
+	const {
+		displayName: username,
+		_json: { email, picture },
+		id
+	} = req.user;
+	const avatar = picture.replace(/=s96-c/g, '?rel=0');
 
+	res.render('dashboard', { avatar, username, email, id });
+});
 
 module.exports = router;
