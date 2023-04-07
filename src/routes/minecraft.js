49c1a310de104ade1fe9e3a39a884c97d077b592
ShiroKazane <bayuofficial773@gmail.com>
2023-04-07 18:08:59 +0700

    Fix bug at routes /avatar

--- src/routes/minecraft.js
+++ src/routes/minecraft.js
@@ -11,7 +11,7 @@ router.get('/avatar/:username', async (req, res) => {
         const username = req.params.username;
         const { id } = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`).then((res) => res.json());
         const avatar = `https://crafatar.com/avatars/${id}?size=${size || '32'}&overlay`;
-        const img = path.resolve(tempPath, `${id}-${size || '32'}.png`);
+        const img = path.resolve(tempPath, `${id || username}-${size || '32'}.png`);
         await getImage(avatar, img);
         res.sendFile(img);
     } catch (err) {
@@ -26,7 +26,7 @@ router.get('/icon/:username', async (req, res) => {
         const username = req.params.username;
         const { id } = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`).then((res) => res.json());
         const icon = `https://visage.surgeplay.com/face/${size || '32'}/${id || 'X-Steve'}.png`;
-        const img = path.resolve(tempPath, `${id || 'X-Steve'}-${size || '32'}.png`);
+        const img = path.resolve(tempPath, `${id || username}-${size || '32'}.png`);
         await getImage(icon, img);
         res.sendFile(img);
     } catch (err) {
